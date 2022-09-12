cdc4aedc0494f086355bb313907a435cd42743af
David Pardo <pardodavid10@gmail.com>
2022-09-11 19:19:09 -0500

    add invalid date handling

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const isNumber = (numberInString) => !isNaN(Number(numberInString));
 // get parsed data
 app.get("/api/:date", (req, res) => {
   const { date } = req.params;
-  if (new Date(date).getTime() === null) {
+  if (new Date(date).getTime() !== null) {
     const parsedDate = new Date(isNumber(date) ? Number(date) : date);
     return res.json({
       unix: parsedDate.getTime(),
